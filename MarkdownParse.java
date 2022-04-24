9d442bac12a8b5c2c9149a9536eaa77179706266
Gregory-Chan <gac826@gmail.com>
2022-04-24 10:03:25 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,7 +17,7 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            currentIndex = currentIndex + closeParen + 1;
         }
 
         return toReturn;
